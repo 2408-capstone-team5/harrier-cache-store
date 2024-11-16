781fbcf0d8867a87aab11fc9539bfa5e92059cbc
Jesse Kercheval <56614846+jessekerch@users.noreply.github.com>
2024-11-16 12:37:57 -0800

    Update harrier-cache-store.sh

--- harrier-cache-store.sh
+++ harrier-cache-store.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Define directory paths
+CACHE_KEY_DIR="../../../s3bucket/node_modules_cache_key"
+CACHE_TAR_DIR="../../../s3bucket/node_modules_cached_tar"
+
+# Ensure the required directories exist
+mkdir -p "$CACHE_KEY_DIR"
+mkdir -p "$CACHE_TAR_DIR"
+
 # Check if cache key exists
-if ls ../../../s3bucket/node_modules_cache_key/${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}*.txt 1> /dev/null 2>&1; then
-    echo "cache key file exists."
+if ls "$CACHE_KEY_DIR/${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}"*.txt 1> /dev/null 2>&1; then
+    echo "Cache key file exists."
     echo "KEY_EXISTS=true" >> $GITHUB_ENV
 else
-    echo "cache key file does not exist."
+    echo "Cache key file does not exist."
     echo "KEY_EXISTS=false" >> $GITHUB_ENV
 fi
 
@@ -14,7 +22,7 @@ source $GITHUB_ENV
 
 # Get latest cache key if it exists
 if [ "$KEY_EXISTS" == "true" ]; then
-    latest_key_file=$(ls ../../../s3bucket/node_modules_cache_key/${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}*.txt | sort -t'_' -k2,2 -r | head -n 1)
+    latest_key_file=$(ls "$CACHE_KEY_DIR/${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}"*.txt | sort -t'_' -k2,2 -r | head -n 1)
     echo "Latest file: $latest_key_file"
     echo "LATEST_KEY=$latest_key_file" >> $GITHUB_ENV
 fi
@@ -24,11 +32,11 @@ source $GITHUB_ENV
 
 # Check if cache key matches
 if [ "$KEY_EXISTS" == "true" ]; then
-    if [ "$(cat $LATEST_KEY)" == "$HASH_FILES_PACKAGE_JSON" ]; then
-        echo "cache key matches - it has not changed."
+    if [ "$(cat "$LATEST_KEY")" == "$HASH_FILES_PACKAGE_JSON" ]; then
+        echo "Cache key matches - it has not changed."
         echo "CACHE_MATCH=true" >> $GITHUB_ENV
     else
-        echo "cache key does not match - it has changed."
+        echo "Cache key does not match - it has changed."
         echo "CACHE_MATCH=false" >> $GITHUB_ENV
     fi
 else
@@ -41,12 +49,12 @@ source $GITHUB_ENV
 
 # Cache current node_modules directory if no cache key exists or if cache key does not match
 if [ "$KEY_EXISTS" == "false" ] || ([ "$KEY_EXISTS" == "true" ] && [ "$CACHE_MATCH" == "false" ]); then
-    echo "creating new cached tar file."
-    tar -czvf ${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}.tar.gz node_modules
-    cp ${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}.tar.gz ../../../s3bucket/node_modules_cached_tar/
+    echo "Creating new cached tar file."
+    tar -czvf "${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}.tar.gz" node_modules
+    mv "${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}.tar.gz" "$CACHE_TAR_DIR/"
 
     # Create new cache key
     TIMESTAMP=$(date +'%Y-%m-%d-%H-%M-%S')
-    echo "create new cache key with timestamp: $TIMESTAMP"
-    echo "$HASH_FILES_PACKAGE_JSON" > ../../../s3bucket/node_modules_cache_key/${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}_${TIMESTAMP}.txt
+    echo "Creating new cache key with timestamp: $TIMESTAMP"
+    echo "$HASH_FILES_PACKAGE_JSON" > "$CACHE_KEY_DIR/${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}_${TIMESTAMP}.txt"
 fi
